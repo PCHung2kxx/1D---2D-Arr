3974384525dca34c0852bd9b723ebb148cd5ba31
PCHung2kxx <dannang2035@gmail.com>
2024-06-19 09:31:29 +0700

    Create BT3 2d.py

---
+++ BT3 2d.py
@@ -0,0 +1,22 @@
+m,n=map(int,input().split())
+
+l=[]
+for i in range(m):
+    l2=[]
+    for j in range(n):
+        k=int(input())
+        l2.append(k)
+    l.append(l2)
+
+def chuyenvi(mat):
+    cot=len(mat[0])
+    hang=len(mat)
+    newmat=[]
+    for i in range(cot):
+        l=[]
+        for j in range(hang):
+            l.append(mat[j][i])
+        newmat.append(l)
+    return newmat
+
+print(chuyenvi(l))
